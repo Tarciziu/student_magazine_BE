304c511b0c7c63be91c95a4bfb852c98cb4e4893
Tarciziu <tarciziu99christin@yahoo.com>
2021-12-27 23:17:08 +0200

    user registration

--- src/main/java/main/controller/UserController.java
+++ src/main/java/main/controller/UserController.java
@@ -1,7 +1,12 @@
 package main.controller;
 
+import main.domain.User;
+import main.exception.ServiceException;
 import main.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) {
+        try {
+            return ResponseEntity.ok().body(userService.register(user));
+        } catch (ServiceException e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/main/service/UserService.java
+++ src/main/java/main/service/UserService.java
@@ -1,6 +1,10 @@
 package main.service;
 
+import main.domain.User;
+import main.exception.ServiceException;
 import main.repository.UserRepository;
+import main.validator.UserValidator;
+import main.validator.ValidationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +13,25 @@ public class UserService implements IUserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserValidator userValidator;
 
     @Override
     public String login(String email, String password) {
         return null;
+    }
+
+    @Override
+    public String register(User user) throws ServiceException {
+        if (userRepository.findById(user.getEmail()).isPresent()) {
+            throw new ServiceException(ServiceException.ErrorCode.VALIDATION, "Email already used");
+        }
+        ValidationResponse userValidation = userValidator.validate(user);
+        if (userValidation.getIsValid()) {
+                userRepository.save(user);
+                return "Success";
+        } else {
+            throw new ServiceException(ServiceException.ErrorCode.VALIDATION, userValidation.getMessages().toString());
+        }
     }
 }
